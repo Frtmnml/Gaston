92cc79b1ec20da1d5360097605c6667b6990d598
Joshua Herrera <42598687+Frtmnml@users.noreply.github.com>
2024-11-28 03:09:57 +0000

    expo tutorial chapter 4

---
+++ StickerSmash/app/(tabs)/index.tsx
@@ -0,0 +1,69 @@
+import Button from "@/components/Button";
+import ImageViewer from "@/components/ImageViewer";
+import { Text, View, StyleSheet } from "react-native";
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from "react";
+
+
+const placeholderImage = require('@/assets/images/background-image.png')
+
+export default function Index() {
+  
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if(!result.canceled){
+      setSelectedImage(result.assets[0].uri)
+    } else{
+      alert('You did not select any image.')
+    }
+
+
+  }
+
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={placeholderImage} selectedImage={selectedImage}/>
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label='Choose a photo' onPress={pickImageAsync}/>
+        <Button label='Use this photo' />
+      </View>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#FFF',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#FFF111',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  }
+})
